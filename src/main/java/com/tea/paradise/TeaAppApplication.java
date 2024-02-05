8cfb356a0ab28b45332b22f51e5696521d7754bb
Kirpich <sob@mail.ru>
2024-02-05 19:39:48 +0300

    start.

---
+++ src/main/java/com/tea/paradise/TeaAppApplication.java
@@ -0,0 +1,13 @@
+package com.tea.paradise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tea/paradise/TeaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tea.paradise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
